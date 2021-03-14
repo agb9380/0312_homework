74b3f3282091fc2fc07f167e45439cc4ae93719b
agb9380 <72142978+agb9380@users.noreply.github.com>
2021-03-14 13:25:57 +0900

    Update Qus6.java

--- Qus6.java
+++ Qus6.java
@@ -29,9 +29,9 @@ public class Qus6 {
 				//System.out.println("1~" + (input - 1)); 초반에 생각했던 것, 입력하고 값을 바로 보여주려고함, 그런데 입력값 input 하나로 여러 번 사용해야함, 처음에 input2, input3이런식으로 사용함..
 				end = input-1;    // 위에 처럼 범위 출력을 한 번더 하게됨, 위 값처럼 출력하면, while 밑에 출력문과 겹쳐서 2번 출력됨
 
-				if (input < 0 || input > 101) {
-					i++;
-					System.out.println("1~100사이의 수를 입력해주세요. ");
+				//if (input < 0 || input > 101) { 이 3행은 필요없는 값.. input2,input3이런식으로 변수 추가할 필요없으니까..
+					//i++;
+					//System.out.println("1~100사이의 수를 입력해주세요. "); //1~100 범위 벗어나면 여기 출력하고, 다시 while 반목문 처음부터 실행하고, int input 입력받을 준비를 함
 				}
 			} else if (randomNum > input) {
 				System.out.println(input + "보다 큰 수 입니다.");
@@ -39,9 +39,9 @@ public class Qus6 {
 				//System.out.println((input + 1) + "~100: ");
 				start = input +1;
 
-				if (input < 0 || input > 101) {
-					i++;
-					System.out.println("1~100사이의 수를 입력해주세요. ");
+				//if (input < 0 || input > 101) {
+					//i++;
+					//System.out.println("1~100사이의 수를 입력해주세요. ");  이 3행은 필요없는 값.. input2,input3이런식으로 변수 추가할 필요없으니까..
 				}
 
 			} else if (input == randomNum) {
@@ -60,37 +60,3 @@ public class Qus6 {
 		}
 	}
 }
-
-/*
- * while (i > 0) { if (randomNum > input) { System.out.println(input +
- * "보다 큰 수 입니다."); System.out.println("기회는" + i + "번 남았습니다.");
- * System.out.println((input + 1) + "~100: "); int input2 = sc.nextInt();
- * 
- * if (randomNum == input2) { System.out.println("정답입니다."); break; } else if
- * (randomNum > input2) { System.out.println(input2 + "보다 큰 수 입니다.");
- * System.out.println("기회는" + i + "번 남았습니다."); System.out.println((input2 + 1) +
- * "~100: "); int input3 = sc.nextInt();
- * 
- * if (randomNum == input3) { System.out.println("정답입니다."); } else {
- * System.out.println("아쉽습니다.. 기회를 다 소진하셨습니다"); System.out.println("정답은" +
- * randomNum); }
- * 
- * }
- * 
- * } else if (randomNum < input) { System.out.println(input + "보다 작은 수 입니다.");
- * System.out.println("기회는" + i + "번 남았습니다."); System.out.println("1~" + (input
- * - 1)); int input2 = sc.nextInt();
- * 
- * if (randomNum == input2) { System.out.println("정답입니다."); break; } else if
- * (randomNum > input2) { System.out.println(input2 + "보다 큰 수 입니다.");
- * System.out.println("기회는" + i + "번 남았습니다."); System.out.println("1~" + (input2
- * - 1)); int input3 = sc.nextInt();
- * 
- * if (randomNum == input3) { System.out.println("정답입니다."); break; } else {
- * System.out.println("아쉽습니다.. 기회를 다 소진하셨습니다"); System.out.println("정답은" +
- * randomNum); } i--; } } } }
- * 
- * }
- * 
- * }
- */
